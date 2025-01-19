8f0c0c8a29bd97f7e7ddaef464084d1041151ec4
Simon Prosen <38320875+simpros@users.noreply.github.com>
2025-01-19 16:56:57 +0100

    added CodeMirror

--- web/src/routes/app/t/[taskid]/task-form.svelte
+++ web/src/routes/app/t/[taskid]/task-form.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
 	import * as Form from '$lib/components/ui/form/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
+	import { StreamLanguage } from '@codemirror/language';
+	import { shell } from '@codemirror/legacy-modes/mode/shell';
+	import { EditorView } from '@codemirror/view';
+	import CodeMirror from 'svelte-codemirror-editor';
 	import { type Infer, superForm, type SuperValidated } from 'sveltekit-superforms';
 	import { valibotClient } from 'sveltekit-superforms/adapters';
 	import { taskSchema, type TaskSchema } from './task-schema';
 
 	let { data }: { data: { form: SuperValidated<Infer<TaskSchema>> } } = $props();
+	let myTheme = EditorView.theme(
+		{
+			'&': {
+				backgroundColor: 'hsl(var(--background) / var(--tw-bg-opacity, 1))',
+				color: 'inherit'
+			},
+			'.cm-gutters': {
+				backgroundColor: 'hsl(var(--muted) / var(--tw-bg-opacity, 1))',
+				color: 'hsl(var(--foreground) / 0.8)',
+				borderWidth: '0',
+				borderRadius: '6px'
+			}
+		},
+		{ dark: true }
+	);
 
 	const form = superForm(data.form, {
 		validators: valibotClient(taskSchema)
@@ -22,7 +41,21 @@
 				<Input {...props} bind:value={$formData.name} />
 			{/snippet}
 		</Form.Control>
-		<Form.Description>This is your public display name.</Form.Description>
+		<Form.FieldErrors />
+	</Form.Field>
+	<Form.Field {form} name="command">
+		<Form.Control>
+			{#snippet children({ props })}
+				<Form.Label>Command</Form.Label>
+				<CodeMirror
+					{...props}
+					class="[&>.cm-editor]:rounded-md [&>.cm-editor]:border [&>.cm-editor]:border-black"
+					theme={myTheme}
+					bind:value={$formData.command}
+					extensions={[StreamLanguage.define(shell)]}
+				/>
+			{/snippet}
+		</Form.Control>
 		<Form.FieldErrors />
 	</Form.Field>
 	<Form.Button>Submit</Form.Button>
--- web/src/routes/app/t/[taskid]/task-schema.ts
+++ web/src/routes/app/t/[taskid]/task-schema.ts
@@ -2,8 +2,8 @@ import * as v from 'valibot';
 
 export const taskSchema = v.object({
 	id: v.string(),
-	name: v.string(),
-	command: v.string(),
+	name: v.pipe(v.string(), v.minLength(1)),
+	command: v.pipe(v.string(), v.minLength(1)),
 	createdAt: v.optional(v.date()),
 	updatedAt: v.optional(v.date())
 });
